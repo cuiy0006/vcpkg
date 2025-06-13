610682975b5b9c9ae8486b5b0211d0f11cfabb6b
Yao Cui <cuiyao@google.com>
2025-06-13 21:02:28 +0000

    pin baseline

--- ports/google-cloud-cpp/portfile.cmake
+++ ports/google-cloud-cpp/portfile.cmake
@@ -63,6 +63,7 @@ vcpkg_cmake_configure(
         -DGOOGLE_CLOUD_CPP_ENABLE_EXAMPLES=OFF
         -DBUILD_TESTING=OFF
         -DGOOGLE_CLOUD_CPP_WITH_MOCKS=OFF
+        -DWITH_ABSEIL=ON
 )
 
 vcpkg_cmake_install()
